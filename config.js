2ed0bee2236b9b1d261809a8e9f91ad0e208bd73
Pinless11 <batrand438@gmail.com>
2024-08-25 10:03:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -23,13 +23,13 @@ global.location = '1'; // Replace with your location panel
 //===========================//
 
 global.prefa = ['', '!', '.', ',', '🐤', '🗿'];
-global.owner = ['2348133729715'];
-global.ownMain = '2348133729715';
-global.NamaOwner = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change 
+global.owner = ['254727204794'];
+global.ownMain = '254727204794';
+global.NamaOwner = 'Pinless'; // Do not change 
 global.sessionName = 'session';
 global.connect = true; // Set to false if using QR Code
 global.namabot = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change
-global.author = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change
+global.author = 'Pinless'; // Do not change
 global.packname = 'PATRON 🚹'; // Change if needed
 global.url = 'https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q'; // Do not change
 
